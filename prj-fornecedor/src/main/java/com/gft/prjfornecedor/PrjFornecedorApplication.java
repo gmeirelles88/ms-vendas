8f52a27e418747a6d947df8cbb8f20de6fa647b8
gmeirelles88 <gabriel.meirelles@etec.sp.gov.br>
2021-03-29 16:45:49 -0300

    Projeto Fornecedor Criado

---
+++ prj-fornecedor/src/main/java/com/gft/prjfornecedor/PrjFornecedorApplication.java
@@ -0,0 +1,13 @@
+package com.gft.prjfornecedor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrjFornecedorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrjFornecedorApplication.class, args);
+	}
+
+}
---
+++ prj-fornecedor/src/test/java/com/gft/prjfornecedor/PrjFornecedorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gft.prjfornecedor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrjFornecedorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
